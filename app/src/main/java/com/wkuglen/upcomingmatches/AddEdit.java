8a0bff1bb9d8dc079f027abf289efc8e93506e7e
wkuglen <wkuglen@gmail.com>
2015-02-08 19:44:42 -0600

    Commit for yesterday. A lot of basic file set-up

--- app/src/main/java/com/wkuglen/upcomingmatches/AddEdit.java
+++ app/src/main/java/com/wkuglen/upcomingmatches/AddEdit.java
@@ -1,25 +1,24 @@
 package com.wkuglen.upcomingmatches;
 
-import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class MainActivity extends ActionBarActivity {
+public class AddEdit extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_add_edit);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_add_edit, menu);
         return true;
     }
 
@@ -31,16 +30,10 @@ public class MainActivity extends ActionBarActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_add) {
-            gotoAddEdit();
+        if (id == R.id.action_settings) {
             return true;
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    private void gotoAddEdit() {
-        Intent intent = new Intent(this, AddEdit.class);
-        startActivity(intent);
     }
 }
