4d47a78809b4af80094c5684b68dc7271a6d1dfa
wkuglen <wkuglen@gmail.com>
2015-02-12 18:06:16 -0600

    Successfully added a time input, number input, and Alliance input.

--- app/src/main/java/com/wkuglen/upcomingmatches/AddEdit.java
+++ app/src/main/java/com/wkuglen/upcomingmatches/AddEdit.java
@@ -1,10 +1,17 @@
 package com.wkuglen.upcomingmatches;
 
+import android.app.Dialog;
+import android.app.TimePickerDialog;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.text.format.DateFormat;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
 
 
 public class AddEdit extends ActionBarActivity {
@@ -13,6 +20,10 @@ public class AddEdit extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_edit);
+
+        DialogFragment newFragment = new TimePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "timePicker");
+
     }
 
 
@@ -39,5 +50,24 @@ public class AddEdit extends ActionBarActivity {
     }
 
     public void submitNewMatch(View view) {
+    }
+}
+
+class TimePickerFragment extends DialogFragment implements TimePickerDialog.OnTimeSetListener {
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Use the current time as the default values for the picker
+        final Calendar c = Calendar.getInstance();
+        int hour = c.get(Calendar.HOUR_OF_DAY);
+        int minute = c.get(Calendar.MINUTE);
+
+        // Create a new instance of TimePickerDialog and return it
+        return new TimePickerDialog(getActivity(), this, hour, minute,
+                DateFormat.is24HourFormat(getActivity()));
+    }
+
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+        // Do something with the time chosen by the user
     }
 }
